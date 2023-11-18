fb3f958ba35ecdf57d6630da10c7ab4f429e3be5
KeoFoxy <arasdiel68@gmail.com>
2023-11-18 15:30:44 +0300

    React routing

--- foxgres-ui/src/App.tsx
+++ foxgres-ui/src/App.tsx
@@ -1,12 +1,34 @@
-import { Fragment, useState } from 'react'
+import { Routes, Route } from 'react-router-dom';
+import {
+  Login,
+  Home,
+  Profile,
+  ErrorPage,
+} from './pages';
+
+import {
+  Layout,
+} from './components';
+
 import './App.css'
-import LogInView from './screens'
 
 function App() {
   return (
-    <Fragment>      
-      <LogInView />
-    </Fragment>
+    <Routes>
+      <Route path='/' element={<Layout />}>
+        {/* Login Page */}
+        <Route path='login' element={<Login />} />
+
+        {/* Main Pages */}
+        {/* <Route element={<RequireAuth />}>   */}
+          <Route path='home' element={<Home />} />
+          <Route path='profile' element={<Profile />} />
+        {/* </Route> */}
+
+        {/* Error route */}
+        <Route path='*' element={<ErrorPage />}/>
+      </Route>
+    </Routes>
   )
 }
 
---
+++ foxgres-ui/src/hooks/useAuth.ts
@@ -0,0 +1,10 @@
+// TODO: WIP
+
+import { useContext } from "react";
+import AuthContext from "../context/AuthProvider";
+
+const useAuth = () => {
+    return useContext(AuthContext);
+};
+
+export default useAuth;
