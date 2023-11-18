cc2014be27ebd58659517197163f012bcf8f159b
jstalex <a1s3rp.v@gmail.com>
2023-11-18 20:34:22 +0300

    back: add swagger

--- foxgres/cmd/foxgres/handler/response.go
+++ foxgres/cmd/foxgres/handler/response.go
@@ -2,4 +2,8 @@ package handler
 
 type AccessLevel struct {
 	AccessLevel int `json:"access_level,omitempty"`
-}
+} // @name AccessLevel
+
+type ResponseErr struct {
+	Message string `json:"message,omitempty"`
+} // @name ResponseErr
